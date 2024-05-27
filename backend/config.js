53493967b79dbe0b894daa5ddbc92c16670a8a87
anishs <anish2sinha@gmail.com>
2024-05-27 17:17:50 -0400

    connected to MongoDB Database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://root:root@bookstore-data.swk95j4.mongodb.net/?retryWrites=true&w=majority&appName=bookstore-data'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to bookstore');
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App listening on PORT: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    });

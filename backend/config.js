0f722181794059692bc0c008e458b8c313bd7984
Anish Sinha <108232600+Anish701@users.noreply.github.com>
2024-06-13 09:23:35 -0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-    'mongodb+srv://root:root@bookstore-data.swk95j4.mongodb.net/?retryWrites=true&w=majority&appName=bookstore-data'
+export const mongoDBURL = DB_URL;
